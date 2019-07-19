17e4d25ac1074039a7ca3caa4125ba16be2981fd
Ashley Williams <ashley666ashley@gmail.com>
2019-07-19 10:39:07 -0500

    feat(wasm-opt): use install mod instead of custom

--- src/wasm_opt.rs
+++ src/wasm_opt.rs
@@ -1,11 +1,9 @@
 //! Support for downloading and executing `wasm-opt`
 
 use crate::child;
-use crate::emoji;
-use crate::target;
+use crate::install;
 use crate::PBAR;
 use binary_install::Cache;
-use log::debug;
 use std::path::{Path, PathBuf};
 use std::process::Command;
 
@@ -66,43 +64,8 @@ pub enum WasmOpt {
 /// have precompiled binaries. Returns an error if we failed to download the
 /// binary.
 pub fn find_wasm_opt(cache: &Cache, install_permitted: bool) -> Result<WasmOpt, failure::Error> {
-    // First attempt to look up in PATH. If found assume it works.
-    if let Ok(path) = which::which("wasm-opt") {
-        debug!("found wasm-opt at {:?}", path);
-        return Ok(WasmOpt::Found(path));
-    }
-
-    // ... and if that fails download a precompiled version.
-    let target = if target::LINUX && target::x86_64 {
-        "x86_64-linux"
-    } else if target::MACOS && target::x86_64 {
-        "x86_64-apple-darwin"
-    } else if target::WINDOWS && target::x86_64 {
-        "x86_64-windows"
-    } else {
-        return Ok(WasmOpt::PlatformNotSupported);
-    };
-    let url = format!(
-        "https://github.com/WebAssembly/binaryen/releases/download/{vers}/binaryen-{vers}-{target}.tar.gz",
-        vers = "version_78",
-        target = target,
-    );
-
-    let download = |permit_install| cache.download(permit_install, "wasm-opt", &["wasm-opt"], &url);
-
-    let dl = match download(false)? {
-        Some(dl) => dl,
-        None if !install_permitted => return Ok(WasmOpt::CannotInstall),
-        None => {
-            let msg = format!("{}Installing wasm-opt...", emoji::DOWN_ARROW);
-            PBAR.info(&msg);
-
-            match download(install_permitted)? {
-                Some(dl) => dl,
-                None => return Ok(WasmOpt::CannotInstall),
-            }
-        }
-    };
-
+    let version = "version_78";
+    let dl =
+        install::download_prebuilt(&install::Tool::WasmOpt, cache, version, install_permitted)?;
     Ok(WasmOpt::Found(dl.binary("wasm-opt")?))
 }
